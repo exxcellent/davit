07e0e2f34d1b7b814a6dff67a893fa50c6814c76
amueller <andreas.mueller@exxcellent.de>
2020-04-06 08:57:18 +0200

    remove default reducer and add metaComponentModel reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,10 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { metaComponentModelReducer } from "../components/MetaComponentModel/ViewModel/MetaComponentModelSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
-  },
+    metaComponentModel: metaComponentModelReducer
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
