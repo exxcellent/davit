d50094417bbbf4b63e0c4c16011267861357092c
amueller <andreas.mueller@exxcellent.de>
2020-04-08 10:04:49 +0200

    add icons lib to project.

--- src/app/Carv2.tsx
+++ src/app/Carv2.tsx
@@ -4,11 +4,17 @@ import { FormattedMessage } from "react-intl";
 import { MetaComponentModelController } from "../components/MetaComponentModel/Presentation/MetaComponentModelController";
 import { ThemeProvider } from "styled-components";
 import { GlobalTheme } from "../style/theme/GlobalTheme";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fab } from "@fortawesome/free-brands-svg-icons";
+import { faPlusSquare, faEllipsisH } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fab, faPlusSquare, faEllipsisH);
 
 export function Carv2() {
   return (
     <div className="Carv2">
       <ThemeProvider theme={GlobalTheme}>
+        <label>Sprache: </label>
         <FormattedMessage id="language"></FormattedMessage>
         <MetaComponentModelController />
       </ThemeProvider>
