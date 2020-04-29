59eaabd03da1d40aa921ff9780f17505fc75db87
amueller <andreas.mueller@exxcellent.de>
2020-04-29 08:46:19 +0200

    implement DataAccess interface.

---
+++ src/dataAccess/DataAccess.ts
@@ -0,0 +1,54 @@
+import { ComponentCTO } from "./access/cto/ComponentCTO";
+import { SequenceCTO } from "./access/cto/SequenceCTO";
+import { SequenceTO } from "./access/to/SequenceTO";
+import { DataAccessResponse } from "./DataAccessResponse";
+import dataStore from "./DataStore";
+import { ComponentDataAccessService } from "./services/ComponentDataAccessService";
+import { SequenceDataAccessService } from "./services/SequenceDataAccessService";
+
+export const DataAccess = {
+  findAllComponents(): DataAccessResponse<ComponentCTO[]> {
+    console.info("Called findAllComponents");
+    return makeTransactional(ComponentDataAccessService.findAll);
+  },
+
+  saveComponentCTO(component: ComponentCTO): DataAccessResponse<ComponentCTO> {
+    console.info("Called saveComponentCTO");
+    return makeTransactional(() => ComponentDataAccessService.save(component));
+  },
+
+  deleteComponentCTO(
+    component: ComponentCTO
+  ): DataAccessResponse<ComponentCTO> {
+    console.info("Called deleteComponentCTO");
+    return makeTransactional(() =>
+      ComponentDataAccessService.delete(component)
+    );
+  },
+
+  findAllSequences(): DataAccessResponse<SequenceTO[]> {
+    return makeTransactional(SequenceDataAccessService.findAll);
+  },
+
+  findSequence(sequenceId: number): DataAccessResponse<SequenceCTO> {
+    return makeTransactional(() => SequenceDataAccessService.find(sequenceId));
+  },
+};
+
+function makeTransactional<T>(callback: () => T): DataAccessResponse<T> {
+  let response: DataAccessResponse<T> = {
+    object: {} as T,
+    message: "",
+    code: 500,
+  };
+  try {
+    response.object = JSON.parse(JSON.stringify(callback()));
+    response.code = 200;
+    dataStore.commitChanges();
+  } catch (error) {
+    console.warn(error);
+    response.message = error.message;
+    dataStore.roleBack();
+  }
+  return response;
+}
