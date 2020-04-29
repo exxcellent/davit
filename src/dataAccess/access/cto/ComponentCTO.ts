131079a62ecdc8d26b55d93f68785d31beca185b
amueller <andreas.mueller@exxcellent.de>
2020-04-29 08:42:51 +0200

    moved TOs and CTOs in seperate files. expand mockData.json

---
+++ src/dataAccess/access/cto/ComponentCTO.ts
@@ -0,0 +1,11 @@
+import { ComponentTO } from "../to/ComponentTO";
+import { DesignTO } from "../to/DesignTO";
+import { GeometricalDataCTO } from "./GeometraicalDataCTO";
+
+export class ComponentCTO {
+  constructor(
+    public component: ComponentTO = new ComponentTO(),
+    public geometricalData: GeometricalDataCTO = new GeometricalDataCTO(),
+    public design: DesignTO = new DesignTO()
+  ) {}
+}
