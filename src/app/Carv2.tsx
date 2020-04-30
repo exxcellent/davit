1615199de65f50e9c19af046cb1d2a3b5324d0bf
amueller <andreas.mueller@exxcellent.de>
2020-04-30 08:50:56 +0200

    removed global theme.

--- src/app/Carv2.tsx
+++ src/app/Carv2.tsx
@@ -1,4 +1,3 @@
-import { ThemeProvider } from "@chakra-ui/core";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { faEllipsisH, faPlusSquare } from "@fortawesome/free-solid-svg-icons";
@@ -6,7 +5,6 @@ import React from "react";
 import { FormattedMessage } from "react-intl";
 import { ControllPanelController } from "../components/controllPanel/ControllPanelController";
 import { MetaComponentModelController } from "../components/MetaComponentModel/Presentation/MetaComponentModelController";
-import { customTheme } from "../style/theme/customTheme";
 
 library.add(fab, faPlusSquare, faEllipsisH);
 
@@ -14,13 +12,11 @@ export function Carv2() {
   return (
     <div className="Carv2">
       <React.StrictMode>
-        <ThemeProvider theme={customTheme}>
-          <ControllPanelController>
-            <label>Sprache: </label>
-            <FormattedMessage id="language"></FormattedMessage>
-          </ControllPanelController>
-          <MetaComponentModelController />
-        </ThemeProvider>
+        <ControllPanelController>
+          <label>Sprache: </label>
+          <FormattedMessage id="language"></FormattedMessage>
+        </ControllPanelController>
+        <MetaComponentModelController />
       </React.StrictMode>
     </div>
   );
