0aa7a7050ec1eddb45e2d959ecf31a558f2ee838
amueller <andreas.mueller@exxcellent.de>
2020-05-05 08:06:23 +0200

    implement upload method.

--- src/components/controllPanel/presentation/fragments/ControllPanelMetaComponentOptions.tsx
+++ src/components/controllPanel/presentation/fragments/ControllPanelMetaComponentOptions.tsx
@@ -1,13 +1,41 @@
 import React, { FunctionComponent } from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Modal } from "semantic-ui-react";
 
 export interface ControllPanelMetaComponentOptionsProps {}
 
 export const ControllPanelMetaComponentOptions: FunctionComponent<ControllPanelMetaComponentOptionsProps> = (
   props
 ) => {
+  const readFileToJSON = (file: File | null) => {
+    const fileReader = new FileReader();
+    let content: string;
+    if (file !== null) {
+      fileReader.readAsText(file);
+      fileReader.onload = (event) => {
+        console.log("result: " + event.target!.result);
+        content = JSON.parse(event.target!.result as string);
+        console.warn("content: " + JSON.stringify(content));
+        const STORE_ID = "carv2";
+        localStorage.setItem(STORE_ID, JSON.stringify(content));
+      };
+      // content = fileReader.readAsText(event.target.result);
+    }
+  };
+
   return (
     <div>
+      <Modal trigger={<Button icon="cloud upload" />}>
+        <input
+          type="file"
+          onChange={(event) => {
+            // TODO: FileHandler Klasse erstellen und ins Backend stecken!
+            if (event.target.files !== null) {
+              readFileToJSON(event.target.files[0]);
+            }
+          }}
+        />
+      </Modal>
+      <Button icon="download" />
       <Button icon="add"></Button>
     </div>
   );
