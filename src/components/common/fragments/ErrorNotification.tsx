83b42c25bf1ee1e41103185563a4911aa224cc62
amueller <andreas.mueller@exxcellent.de>
2020-05-06 08:34:52 +0200

    moved error notification to bottom.

---
+++ src/components/common/fragments/ErrorNotification.tsx
@@ -0,0 +1,32 @@
+import React, { FunctionComponent } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Button, Message, Transition } from "semantic-ui-react";
+import { globalSlice, selectGlobalErrorState } from "../viewModel/GlobalSlice";
+
+export interface ErrorNotificationProps {}
+
+export const ErrorNotification: FunctionComponent<ErrorNotificationProps> = (
+  props
+) => {
+  const errorMessages: string[] = useSelector(selectGlobalErrorState);
+  const dispatch = useDispatch();
+  const { clearErrors } = globalSlice.actions;
+
+  return (
+    <Transition
+      visible={errorMessages.length > 0}
+      animate="slide up"
+      duration={1000}
+    >
+      <Message error compact>
+        <Message.Header>Error</Message.Header>
+        <Button
+          icon="close"
+          size="mini"
+          onClick={() => dispatch(clearErrors())}
+        />
+        {errorMessages}
+      </Message>
+    </Transition>
+  );
+};
