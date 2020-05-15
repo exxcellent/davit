b660bcc04255f8870368033f8e3cd7eab1e5296b
amueller <andreas.mueller@exxcellent.de>
2020-05-15 07:57:25 +0200

    add new props "direction" realtion type to dataconnection.

--- src/dataAccess/access/to/DataConnectionTO.ts
+++ src/dataAccess/access/to/DataConnectionTO.ts
@@ -1,7 +1,25 @@
 import { AbstractTO } from "./AbstractTO";
 
+export enum Direction {
+  LEFT = "LEFT",
+  RIGHT = "RIGHT",
+  TOP = "TOP",
+  BOTTOM = "BOTTOM",
+}
+
+export enum RelationType {
+  DEFAULT = "DEFAULT",
+}
+
 export class DataConnectionTO extends AbstractTO {
-  constructor(public data1Fk: number = -1, public data2Fk: number = -1) {
+  constructor(
+    public data1Fk: number = -1,
+    public data2Fk: number = -1,
+    public label1: string = "",
+    public label2: string = "",
+    public direction: Direction = Direction.RIGHT,
+    public type: RelationType = RelationType.DEFAULT
+  ) {
     super();
   }
 }
