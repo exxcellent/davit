f9ee61b3ffc9975f5aac59f64a4a51775c28647d
amueller <andreas.mueller@exxcellent.de>
2020-05-18 12:02:22 +0200

    placeholder rows.

--- src/components/sequenceTableModel/presentation/SequenceTableModelController.tsx
+++ src/components/sequenceTableModel/presentation/SequenceTableModelController.tsx
@@ -29,6 +29,7 @@ export const SequenceTableModelController: FunctionComponent<SequenceTableModelC
         id={sequenceStepCTO.squenceStepTO.id}
         active={selectedId === sequenceStepCTO.squenceStepTO.id}
       >
+        <Table.Cell>{sequenceStepCTO.squenceStepTO.id}</Table.Cell>
         <Table.Cell>{sequenceStepCTO.squenceStepTO.name}</Table.Cell>
         <Table.Cell>
           {sequenceStepCTO.componentCTOSource.component.name}
@@ -40,19 +41,38 @@ export const SequenceTableModelController: FunctionComponent<SequenceTableModelC
     );
   };
 
+  const createRowPlaceholder = (index: number) => {
+    return (
+      <Table.Row id={index}>
+        <Table.Cell textAlign="center">{index}</Table.Cell>
+        <Table.Cell></Table.Cell>
+        <Table.Cell></Table.Cell>
+        <Table.Cell></Table.Cell>
+      </Table.Row>
+    );
+  };
+
+  const createTable = (sequence: SequenceCTO | undefined) => {
+    if (sequence !== undefined) {
+      return sequence.sequenceStepCTOs.map(createTableRow);
+    } else {
+      const placeholder: number[] = Array.from(Array(10).keys());
+      return placeholder.map(createRowPlaceholder);
+    }
+  };
+
   return (
     <div className="sequenceTable">
       <Table celled inverted>
         <Table.Header>
           <Table.Row>
+            <Table.HeaderCell style={{ width: "15px" }}>Nr.</Table.HeaderCell>
             <Table.HeaderCell>Name</Table.HeaderCell>
             <Table.HeaderCell>Sender</Table.HeaderCell>
             <Table.HeaderCell>Receiver</Table.HeaderCell>
           </Table.Row>
         </Table.Header>
-        <Table.Body>
-          {sequence?.sequenceStepCTOs.map(createTableRow)}
-        </Table.Body>
+        <Table.Body>{createTable(sequence)}</Table.Body>
       </Table>
     </div>
   );
