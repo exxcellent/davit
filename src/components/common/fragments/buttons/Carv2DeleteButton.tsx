ab6b002b72fc21b73af574b3decaf9dd8239dd02
amueller <andreas.mueller@exxcellent.de>
2020-05-20 11:01:51 +0200

    delete button confirm

--- src/components/common/fragments/buttons/Carv2DeleteButton.tsx
+++ src/components/common/fragments/buttons/Carv2DeleteButton.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { Button } from "semantic-ui-react";
 
 interface Carv2DeleteButtonProps {
@@ -10,13 +10,41 @@ export const Carv2DeleteButton: FunctionComponent<Carv2DeleteButtonProps> = (
 ) => {
   const { onClick } = props;
 
+  const [fluid, setFluid] = useState<boolean>(false);
+
+  const onButtonClick = () => {
+    setFluid(true);
+  };
+
   return (
-    <Button
-      icon="trash alternate"
-      onClick={onClick}
-      className="carv2Button"
-      inverted
-      color="red"
-    />
+    <div
+      style={{
+        display: "flex",
+        width: "100%",
+        justifyContent: "center",
+        paddingRight: "10px",
+      }}
+    >
+      {!fluid && (
+        <Button
+          icon="trash alternate"
+          onClick={onButtonClick}
+          className="carv2Button"
+          inverted
+          color="red"
+        />
+      )}
+      {fluid && (
+        <Button
+          onClick={onClick}
+          className="carv2Button"
+          inverted
+          color="red"
+          fluid
+        >
+          SURE?
+        </Button>
+      )}
+    </div>
   );
 };
