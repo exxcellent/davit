325790eb085dea8bec021b4c6ce5b8c9235f25e1
amueller <andreas.mueller@exxcellent.de>
2020-06-02 08:22:51 +0200

    removed old slices and actions classes.

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,15 +1,11 @@
 import { configureStore, ThunkAction } from "@reduxjs/toolkit";
 import { globalReducer } from "../components/common/viewModel/GlobalSlice";
-import { controllPanelReducer } from "../components/controllPanel/viewModel/ControllPanelSlice";
-import { metaDataModelReducer } from "../components/metaDataModel/viewModel/MetaDataModelSlice";
-import { ComponentReducer } from "../viewModel/ComponentSlice";
-import { DataReducer } from "../viewModel/DataSlice";
-import { SequenceReducer } from "../viewModel/SequenceSlice";
+import { ComponentReducer } from "../slices/ComponentSlice";
+import { DataReducer } from "../slices/DataSlice";
+import { SequenceReducer } from "../slices/SequenceSlice";
 
 export const store = configureStore({
   reducer: {
-    metaDataModel: metaDataModelReducer,
-    controllPannel: controllPanelReducer,
     global: globalReducer,
     sequenceModel: SequenceReducer,
     componentModel: ComponentReducer,
