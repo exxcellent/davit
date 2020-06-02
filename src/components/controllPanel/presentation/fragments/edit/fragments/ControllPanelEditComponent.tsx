6082056b32707db47799bed9a183b267370f14b3
amueller <andreas.mueller@exxcellent.de>
2020-06-02 08:26:02 +0200

    moved controllpaneledit components.

---
+++ src/components/controllPanel/presentation/fragments/edit/fragments/ControllPanelEditComponent.tsx
@@ -0,0 +1,101 @@
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Input } from "semantic-ui-react";
+import { isNullOrUndefined } from "util";
+import { ComponentCTO } from "../../../../../../dataAccess/access/cto/ComponentCTO";
+import { ComponentActions, currentComponent } from "../../../../../../slices/ComponentSlice";
+import { Carv2Util } from "../../../../../../utils/Carv2Util";
+import { Carv2DeleteButton } from "../../../../../common/fragments/buttons/Carv2DeleteButton";
+import { GlobalActions, handleError } from "../../../../../common/viewModel/GlobalSlice";
+import { ControllPanelEditSub } from "../common/ControllPanelEditSub";
+import { Carv2LabelTextfield } from "../common/fragments/Carv2LabelTextfield";
+import { Carv2SubmitCancel } from "../common/fragments/Carv2SubmitCancel";
+
+export interface ControllPanelEditComponentProps {}
+
+export const ControllPanelEditComponent: FunctionComponent<ControllPanelEditComponentProps> = (props) => {
+  const {
+    label,
+    name,
+    changeName,
+    saveComponent,
+    deleteComponent,
+    cancel,
+    toggleIsCreateAnother,
+    textInput,
+    showDelete,
+  } = useControllPanelEditComponentViewModel();
+
+  return (
+    <ControllPanelEditSub label={label}>
+      <div />
+      <Carv2LabelTextfield
+        label="Name:"
+        placeholder="Component Name"
+        onChange={(event: any) => changeName(event.target.value)}
+        value={name}
+        autoFocus
+        ref={textInput}
+      />
+      <div className="columnDivider" style={{ display: "flex" }}>
+        <Carv2SubmitCancel onSubmit={saveComponent} onCancel={cancel} onChange={toggleIsCreateAnother} />
+      </div>
+      {showDelete && (
+        <div className="columnDivider">
+          <div className="controllPanelEditChild" style={{ display: "flex", alignItems: "center", height: "100%" }}>
+            <Carv2DeleteButton onClick={deleteComponent} />
+          </div>
+        </div>
+      )}
+    </ControllPanelEditSub>
+  );
+};
+
+const useControllPanelEditComponentViewModel = () => {
+  const componentToEdit: ComponentCTO | null = useSelector(currentComponent);
+  const dispatch = useDispatch();
+  const [isCreateAnother, setIsCreateAnother] = useState<boolean>(true);
+  const textInput = useRef<Input>(null);
+
+  useEffect(() => {
+    // check if component to edit is really set or gso back to edit mode
+    if (isNullOrUndefined(componentToEdit)) {
+      GlobalActions.setModeToEdit();
+      handleError("Tried to go to edit component without componentToedit specified");
+    }
+    // used to focus the textfield on create another
+    textInput.current!.focus();
+  }, [componentToEdit]);
+
+  const changeName = (name: string) => {
+    let copyComponentToEdit: ComponentCTO = Carv2Util.deepCopy(componentToEdit);
+    copyComponentToEdit.component.name = name;
+    dispatch(ComponentActions.setCompoenentToEdit(copyComponentToEdit));
+  };
+
+  const saveComponent = () => {
+    dispatch(ComponentActions.saveComponent(componentToEdit!));
+    if (isCreateAnother) {
+      dispatch(GlobalActions.setModeToEditComponent());
+    } else {
+      dispatch(GlobalActions.setModeToEdit());
+    }
+  };
+
+  const deleteComponent = () => {
+    dispatch(ComponentActions.deleteComponent(componentToEdit!));
+    dispatch(GlobalActions.setModeToEdit());
+  };
+
+  return {
+    label: componentToEdit!.component.id === -1 ? "ADD COMPONENT" : "EDIT COMPONENT",
+    name: componentToEdit!.component.name,
+    changeName,
+    saveComponent,
+    deleteComponent,
+    cancel: () => dispatch(GlobalActions.setModeToEdit()),
+    toggleIsCreateAnother: () => setIsCreateAnother(!isCreateAnother),
+    textInput,
+    showDelete: componentToEdit!.component.id !== -1,
+  };
+};
---
+++ src/components/controllPanel/presentation/fragments/edit/fragments/ControllPanelEditData.tsx
@@ -0,0 +1,105 @@
+import React, { FunctionComponent, useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Input } from "semantic-ui-react";
+import { isNullOrUndefined } from "util";
+import { DataCTO } from "../../../../../../dataAccess/access/cto/DataCTO";
+import { DataActions, selectCurrentData } from "../../../../../../slices/DataSlice";
+import { Carv2Util } from "../../../../../../utils/Carv2Util";
+import { Carv2DeleteButton } from "../../../../../common/fragments/buttons/Carv2DeleteButton";
+import { GlobalActions, handleError } from "../../../../../common/viewModel/GlobalSlice";
+import { ControllPanelEditSub } from "../common/ControllPanelEditSub";
+import { Carv2LabelTextfield } from "../common/fragments/Carv2LabelTextfield";
+import { Carv2SubmitCancel } from "../common/fragments/Carv2SubmitCancel";
+
+export interface ControllPanelEditDataProps {}
+
+export const ControllPanelEditData: FunctionComponent<ControllPanelEditDataProps> = (props) => {
+  const {
+    label,
+    textInput,
+    cancel,
+    changeName,
+    deleteData,
+    name,
+    saveData,
+    showDelete,
+    toggleIsCreateAnother,
+  } = useControllPanelEditDataViewModel();
+
+  return (
+    <ControllPanelEditSub label={label}>
+      <div />
+      <Carv2LabelTextfield
+        label="Name:"
+        placeholder="Data Name"
+        onChange={(event: any) => changeName(event.target.value)}
+        value={name}
+        autoFocus
+        ref={textInput}
+      />
+      <div className="columnDivider" style={{ display: "flex" }}>
+        <Carv2SubmitCancel onSubmit={saveData} onCancel={cancel} onChange={toggleIsCreateAnother} />
+      </div>
+      {showDelete && (
+        <div className="columnDivider">
+          <div className="controllPanelEditChild" style={{ display: "felx", alignItems: "center", height: "100%" }}>
+            <Carv2DeleteButton onClick={deleteData} />
+          </div>
+        </div>
+      )}
+    </ControllPanelEditSub>
+  );
+};
+
+const useControllPanelEditDataViewModel = () => {
+  const dataToEdit: DataCTO | null = useSelector(selectCurrentData);
+  const dispatch = useDispatch();
+  const [isCreateAnother, setIsCreateAnother] = useState<boolean>(true);
+  const textInput = useRef<Input>(null);
+
+  useEffect(() => {
+    // used to focus the textfield on create another
+    textInput.current!.focus();
+  }, [dataToEdit]);
+
+  useEffect(() => {
+    console.log(dataToEdit);
+    // check if component to edit is really set or gso back to edit mode
+    if (isNullOrUndefined(dataToEdit)) {
+      dispatch(GlobalActions.setModeToEdit());
+      handleError("Tried to go to edit data without dataToedit specified");
+    }
+  });
+
+  const changeName = (name: string) => {
+    let copyDataToEdit: DataCTO = Carv2Util.deepCopy(dataToEdit);
+    copyDataToEdit.data.name = name;
+    dispatch(DataActions.setDataToEdit(copyDataToEdit));
+  };
+
+  const saveData = () => {
+    dispatch(DataActions.saveData(dataToEdit!));
+    if (isCreateAnother) {
+      dispatch(GlobalActions.setModeToEditData());
+    } else {
+      dispatch(GlobalActions.setModeToEdit());
+    }
+  };
+
+  const deleteData = () => {
+    dispatch(DataActions.deleteData(dataToEdit!));
+    dispatch(GlobalActions.setModeToEdit());
+  };
+
+  return {
+    label: dataToEdit?.data.id === -1 ? "ADD DATA" : "EDIT DATA",
+    name: dataToEdit?.data.name,
+    changeName,
+    saveData,
+    deleteData,
+    cancel: () => dispatch(GlobalActions.setModeToEdit()),
+    toggleIsCreateAnother: () => setIsCreateAnother(!isCreateAnother),
+    textInput,
+    showDelete: dataToEdit?.data.id !== -1,
+  };
+};
