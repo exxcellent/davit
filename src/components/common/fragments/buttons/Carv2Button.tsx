0c047c4c55ce472f35b993ce0c33ecf0979d7f19
amueller <andreas.mueller@exxcellent.de>
2020-06-08 16:32:45 +0200

    add submit condition.

--- src/components/common/fragments/buttons/Carv2Button.tsx
+++ src/components/common/fragments/buttons/Carv2Button.tsx
@@ -4,18 +4,11 @@ import { Button } from "semantic-ui-react";
 interface Carv2ButtonProps {
   onClick: () => void;
   icon?: string;
+  disable?: boolean;
 }
 
 export const Carv2Button: FunctionComponent<Carv2ButtonProps> = (props) => {
-  const { onClick, icon } = props;
+  const { onClick, icon, disable } = props;
 
-  return (
-    <Button
-      icon={icon}
-      onClick={onClick}
-      className="carv2Button"
-      inverted
-      color="orange"
-    />
-  );
+  return <Button icon={icon} onClick={onClick} className="carv2Button" inverted color="orange" disabled={disable} />;
 };
--- src/components/controllPanel/presentation/fragments/edit/common/fragments/Carv2SubmitCancel.tsx
+++ src/components/controllPanel/presentation/fragments/edit/common/fragments/Carv2SubmitCancel.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { Carv2Button } from "../../../../../../common/fragments/buttons/Carv2Button";
 import { Carv2Checkbox } from "../../../../../../common/fragments/Carv2Checkbox";
 
@@ -6,16 +6,25 @@ interface Carv2SubmitCancelProps {
   onSubmit: () => void;
   onCancel: () => void;
   onChange: () => void;
+  submitCondition?: boolean;
 }
 
-export const Carv2SubmitCancel: FunctionComponent<Carv2SubmitCancelProps> = (
-  props
-) => {
-  const { onCancel, onChange, onSubmit } = props;
+export const Carv2SubmitCancel: FunctionComponent<Carv2SubmitCancelProps> = (props) => {
+  const { onCancel, onChange, onSubmit, submitCondition } = props;
+
+  const [disable, setDisable] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (submitCondition === undefined) {
+      setDisable(false);
+    } else {
+      setDisable(!submitCondition);
+    }
+  }, [submitCondition]);
 
   return (
     <div className="controllPanelEditChild">
-      <Carv2Button icon="check" onClick={onSubmit} />
+      <Carv2Button icon="check" onClick={onSubmit} disable={disable} />
       <Carv2Button icon="times" onClick={onCancel} />
       <Carv2Checkbox label="Create another" onChange={onChange} />
     </div>
