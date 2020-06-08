0f88b53cde550570be76a0ee7710bc025cf6ca37
amueller <andreas.mueller@exxcellent.de>
2020-06-08 16:32:30 +0200

    regexp for valid name.

--- src/utils/Carv2Util.ts
+++ src/utils/Carv2Util.ts
@@ -2,8 +2,17 @@ import { isNullOrUndefined } from "util";
 
 export const Carv2Util = {
   deepCopy(object: any) {
-    return isNullOrUndefined(object)
-      ? object
-      : JSON.parse(JSON.stringify(object));
+    return isNullOrUndefined(object) ? object : JSON.parse(JSON.stringify(object));
+  },
+
+  isValidName(name: string): boolean {
+    const nameRegExp: RegExp = /^[A-Za-z0-9_./-]+$/;
+    let valid: boolean = false;
+    if (!isNullOrUndefined(name)) {
+      if (!isNullOrUndefined(name)) {
+        valid = nameRegExp.test(name);
+      }
+    }
+    return valid;
   },
 };
