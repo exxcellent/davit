67a8ff17203ffea2008a5f5c356ba3648f2b0b12
amueller <andreas.mueller@exxcellent.de>
2020-06-09 15:22:01 +0200

    refactor view menu.

--- src/components/controllPanel/presentation/fragments/view/ControllPanelSequenceOptions.tsx
+++ src/components/controllPanel/presentation/fragments/view/ControllPanelSequenceOptions.tsx
@@ -6,6 +6,7 @@ import { SequenceCTO } from "../../../../../dataAccess/access/cto/SequenceCTO";
 import { SequenceStepCTO } from "../../../../../dataAccess/access/cto/SequenceStepCTO";
 import { currentSequence, currentStep, SequenceActions, SequenceSlice } from "../../../../../slices/SequenceSlice";
 import { useGetSequenceLabelDropdown } from "../edit/common/fragments/Carv2DropDown";
+import { OptionField } from "../edit/common/OptionField";
 
 export interface ControllPanelSequenceOptionsProps {}
 
@@ -27,33 +28,71 @@ export const ControllPanelSequenceOptions: FunctionComponent<ControllPanelSequen
   };
 
   return (
-    <div className="controllPanelView">
-      <div className="controllPanelViewChild">
-        {useGetSequenceLabelDropdown((sequence) => selectSequence(sequence), "Select Sequence ...")}
+    <div className="controllPanelEdit">
+      <div className="optionFieldSpacer">
+        <OptionField></OptionField>
       </div>
-      <div className="controllPanelViewChild">
-        <Button.Group inverted color="orange">
-          <Button
-            inverted
-            color="orange"
-            icon="left arrow"
-            content="BACK"
-            labelPosition="left"
-            disabled={isNullOrUndefined(sequence)}
-            onClick={() => dispatch(SequenceSlice.actions.setPreviousStepToCurrentStep())}
-          />
-          <Button inverted color="orange" content={step?.squenceStepTO.index || 0} disabled={true} />
-          <Button
-            inverted
-            color="orange"
-            icon="right arrow"
-            content="NEXT"
-            labelPosition="right"
-            disabled={isNullOrUndefined(sequence)}
-            onClick={() => dispatch(SequenceSlice.actions.setNextStepToCurrentStep())}
-          />
-        </Button.Group>
+      <div className="optionFieldSpacer columnDivider">
+        <OptionField>
+          {useGetSequenceLabelDropdown((sequence) => selectSequence(sequence), "Select Sequence ...")}
+        </OptionField>
+      </div>
+      <div className="optionFieldSpacer columnDivider">
+        <OptionField>
+          <Button.Group inverted color="orange">
+            <Button
+              inverted
+              color="orange"
+              icon="left arrow"
+              content="BACK"
+              labelPosition="left"
+              disabled={isNullOrUndefined(sequence)}
+              onClick={() => dispatch(SequenceSlice.actions.setPreviousStepToCurrentStep())}
+            />
+            <Button inverted color="orange" content={step?.squenceStepTO.index || 0} disabled={true} />
+            <Button
+              inverted
+              color="orange"
+              icon="right arrow"
+              content="NEXT"
+              labelPosition="right"
+              disabled={isNullOrUndefined(sequence)}
+              onClick={() => dispatch(SequenceSlice.actions.setNextStepToCurrentStep())}
+            />
+          </Button.Group>
+        </OptionField>
+      </div>
+      <div className="optionFieldSpacer columnDivider">
+        <OptionField></OptionField>
       </div>
     </div>
+    // <div className="controllPanelView">
+    //   <div className="controllPanelViewChild">
+    //     {useGetSequenceLabelDropdown((sequence) => selectSequence(sequence), "Select Sequence ...")}
+    //   </div>
+    //   <div className="controllPanelViewChild">
+    //     <Button.Group inverted color="orange">
+    //       <Button
+    //         inverted
+    //         color="orange"
+    //         icon="left arrow"
+    //         content="BACK"
+    //         labelPosition="left"
+    //         disabled={isNullOrUndefined(sequence)}
+    //         onClick={() => dispatch(SequenceSlice.actions.setPreviousStepToCurrentStep())}
+    //       />
+    //       <Button inverted color="orange" content={step?.squenceStepTO.index || 0} disabled={true} />
+    //       <Button
+    //         inverted
+    //         color="orange"
+    //         icon="right arrow"
+    //         content="NEXT"
+    //         labelPosition="right"
+    //         disabled={isNullOrUndefined(sequence)}
+    //         onClick={() => dispatch(SequenceSlice.actions.setNextStepToCurrentStep())}
+    //       />
+    //     </Button.Group>
+    //   </div>
+    // </div>
   );
 };
