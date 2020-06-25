5528db4814ed5de664ab46f566aeb5530a2d13bb
amueller <andreas.mueller@exxcellent.de>
2020-06-25 11:13:51 +0200

    changed compDataCTO to TO.

--- src/reducer/SequenceActionReducer.ts
+++ src/reducer/SequenceActionReducer.ts
@@ -1,24 +1,24 @@
 import { isNullOrUndefined } from "util";
 import { ActionCTO } from "../dataAccess/access/cto/ActionCTO";
-import { ComponentDataCTO } from "../dataAccess/access/cto/ComponentDataCTO";
+import { ComponentDataTO } from "../dataAccess/access/to/ComponentDataTO";
 import { ActionType } from "../dataAccess/access/types/ActionType";
 import { ComponentDataState } from "../dataAccess/access/types/ComponentDataState";
 import { Carv2Util } from "../utils/Carv2Util";
 
 export interface SequenceActionResult {
-  componenDatas: ComponentDataCTO[];
+  componenDatas: ComponentDataTO[];
   errors: ActionCTO[];
 }
 
 export const SequenceActionReducer = {
-  executeActionsOnComponentDatas(actions: ActionCTO[], componentDatas: ComponentDataCTO[]): SequenceActionResult {
-    let newComponentDatas: ComponentDataCTO[] = Carv2Util.deepCopy(componentDatas);
+  executeActionsOnComponentDatas(actions: ActionCTO[], componentDatas: ComponentDataTO[]): SequenceActionResult {
+    let newComponentDatas: ComponentDataTO[] = Carv2Util.deepCopy(componentDatas);
     let errors: ActionCTO[] = [];
 
     actions.forEach((action) => {
       const index: number = findComponentDataIndex(action.componentTO.id, action.dataTO.id, newComponentDatas);
       // modification
-      let result: { compData: ComponentDataCTO | undefined; errorItem: ActionCTO | null };
+      let result: { compData: ComponentDataTO | undefined; errorItem: ActionCTO | null };
       if (index > -1) {
         result = executeActionOnComponentData(action, newComponentDatas[index]);
         if (!isNullOrUndefined(result.compData)) {
@@ -38,62 +38,49 @@ export const SequenceActionReducer = {
   },
 };
 
-const findComponentDataIndex = (compId: number, dataId: number, componentDatas: ComponentDataCTO[]): number => {
-  return componentDatas.findIndex((compData) => compData.componentTO.id === compId && compData.dataTO.id === dataId);
+const findComponentDataIndex = (compId: number, dataId: number, componentDatas: ComponentDataTO[]): number => {
+  return componentDatas.findIndex((compData) => compData.componentFk === compId && compData.dataFk === dataId);
 };
 
-const createErrorComponentDataFromAction = (action: ActionCTO): ComponentDataCTO => {
+const createErrorComponentDataFromAction = (action: ActionCTO): ComponentDataTO => {
   return {
-    componentDataTO: {
-      id: -1,
-      componentDataState: ComponentDataState.NEW,
-      componentFk: action.componentTO.id,
-      dataFk: action.dataTO.id,
-      sequenceStepFk: action.actionTO.sequenceStepFk,
-    },
-    componentTO: action.componentTO,
-    dataTO: action.dataTO,
+    id: -1,
+    componentDataState: ComponentDataState.NEW,
+    componentFk: action.componentTO.id,
+    dataFk: action.dataTO.id,
+    sequenceStepFk: action.actionTO.sequenceStepFk,
   };
 };
 
 const executeActionOnComponentData = (
   action: ActionCTO,
-  componentData?: ComponentDataCTO
-): { compData: ComponentDataCTO | undefined; errorItem: ActionCTO | null } => {
-  let newCompData: ComponentDataCTO | undefined = Carv2Util.deepCopy(componentData);
+  componentData?: ComponentDataTO
+): { compData: ComponentDataTO | undefined; errorItem: ActionCTO | null } => {
+  let newCompData: ComponentDataTO | undefined = Carv2Util.deepCopy(componentData);
   let errorItem: ActionCTO | null = null;
 
   // create as default an error compData and set the state if its no error
   switch (action.actionTO.actionType) {
     case ActionType.ADD:
-      if (
-        componentData === undefined ||
-        componentData.componentDataTO.componentDataState === ComponentDataState.DELETED
-      ) {
+      if (componentData === undefined || componentData.componentDataState === ComponentDataState.DELETED) {
         newCompData = createErrorComponentDataFromAction(action);
-        newCompData.componentDataTO.componentDataState = ComponentDataState.NEW;
+        newCompData.componentDataState = ComponentDataState.NEW;
       } else {
         errorItem = Carv2Util.deepCopy(action);
       }
       break;
     case ActionType.CHECK:
-      if (
-        componentData !== undefined &&
-        componentData.componentDataTO.componentDataState !== ComponentDataState.DELETED
-      ) {
+      if (componentData !== undefined && componentData.componentDataState !== ComponentDataState.DELETED) {
         newCompData = createErrorComponentDataFromAction(action);
-        newCompData.componentDataTO.componentDataState = ComponentDataState.PERSISTENT;
+        newCompData.componentDataState = ComponentDataState.PERSISTENT;
       } else {
         errorItem = Carv2Util.deepCopy(action);
       }
       break;
     case ActionType.DELETE:
-      if (
-        componentData !== undefined &&
-        componentData.componentDataTO.componentDataState !== ComponentDataState.DELETED
-      ) {
+      if (componentData !== undefined && componentData.componentDataState !== ComponentDataState.DELETED) {
         newCompData = createErrorComponentDataFromAction(action);
-        newCompData.componentDataTO.componentDataState = ComponentDataState.DELETED;
+        newCompData.componentDataState = ComponentDataState.DELETED;
       } else {
         errorItem = Carv2Util.deepCopy(action);
       }
