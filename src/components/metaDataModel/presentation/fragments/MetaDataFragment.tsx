4d1d67853ec1dde9b5aa983ec21c4504fb33f8f3
amueller <andreas.mueller@exxcellent.de>
2020-07-22 13:26:15 +0200

    fixed data tab update problem.

--- src/components/metaDataModel/presentation/fragments/MetaDataFragment.tsx
+++ src/components/metaDataModel/presentation/fragments/MetaDataFragment.tsx
@@ -9,28 +9,35 @@ export interface MetaDataFragmentProps {
   initalWidth?: number;
   initalHeigth?: number;
   componentFragments: ViewFragmentProps[];
-  onClick: (dataId: number) => void;
+  onClick?: (dataId: number) => void;
 }
 
 const MetaDataFragment: FunctionComponent<MetaDataFragmentProps> = (props) => {
   const { initalName, componentFragments, initalWidth, initalHeigth } = props;
 
   return (
-    <Card style={{ width: initalWidth, height: initalHeigth }} onClick={() => props.onClick(props.id)}>
+    <Card
+      style={{ width: initalWidth, height: initalHeigth }}
+      onClick={props.onClick ? () => props.onClick!(props.id) : undefined}
+    >
       <Card.Content header={initalName}></Card.Content>
       {componentFragments.map(createViewFragment)}
     </Card>
   );
 };
 
-export const createMetaDataFragment = (dataCTO: DataCTO, componentDatas: ViewFragmentProps[], onClick: (dataId: number) => void) => {
+export const createMetaDataFragment = (
+  dataCTO: DataCTO,
+  componentDatas: ViewFragmentProps[],
+  onClick: (dataId: number) => void
+) => {
   return (
     <MetaDataFragment
       id={dataCTO.data.id}
       initalName={dataCTO.data.name}
       initalWidth={dataCTO.geometricalData.geometricalData.width}
       componentFragments={componentDatas}
-      onClick={onClick}
+      // onClick={onClick}
     />
   );
 };
