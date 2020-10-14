4f77fba2aef188f9b2faec95c0d3551a7171d4ec
Andreas Müller <lux@Andreass-MacBook-Pro.local>
2020-10-14 16:29:26 +0200

    add todo about router

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { IntlProvider } from "react-intl";
-import { Provider } from "react-redux";
-// import { BrowserRouter } from 'react-router-dom';
-import { HashRouter as BrowserRouter } from "react-router-dom";
-import "semantic-ui-css/semantic.min.css";
-import { Carv2 } from "./app/Carv2";
-import { store } from "./app/store";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import { languages } from "./translations/languages";
+import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { IntlProvider } from 'react-intl';
+import { Provider } from 'react-redux';
+import { HashRouter as BrowserRouter } from 'react-router-dom';
+
+import { Carv2 } from './app/Carv2';
+import { store } from './app/store';
+import * as serviceWorker from './serviceWorker';
+import { languages } from './translations/languages';
+
+// import { BrowserRouter } from 'react-router-dom';
 // TODO: if we rollout to english countries, this must be redefined (changing the locale "de" to "en" already works)
 /* support variables for languages */
 // const locale = navigator.language;
@@ -23,6 +25,7 @@ const localeData = languages[locale];
 ReactDOM.render(
   <Provider store={store}>
     <IntlProvider locale={locale} messages={localeData}>
+      {/* TODO: remove # from url */}
       <BrowserRouter>
         <Carv2 />
       </BrowserRouter>
