a3b6b41d2a3c911d7be25c61b38796ca3d9a0c56
amueller <andreas.mueller@exxcellent.de>
2020-10-30 11:09:06 +0100

    fromatter games

--- src/dataAccess/access/to/DecisionTO.ts
+++ src/dataAccess/access/to/DecisionTO.ts
@@ -1,17 +1,17 @@
-import { DataAndInstanceId } from '../../../components/common/fragments/dropdowns/InstanceDropDown';
-import { GoTo, GoToTypes } from '../types/GoToType';
-import { AbstractTO } from './AbstractTO';
+import {DataAndInstanceId} from '../../../components/common/fragments/dropdowns/InstanceDropDown';
+import {GoTo, GoToTypes} from '../types/GoToType';
+import {AbstractTO} from './AbstractTO';
 
 export class DecisionTO extends AbstractTO {
   constructor(
-    public name: string = "",
+    public name: string = '',
     public sequenceFk: number = -1,
     public actorFk: number = -1,
     public has: boolean = true,
     public dataAndInstaceId: DataAndInstanceId[] = [],
-    public ifGoTo: GoTo = { type: GoToTypes.FIN },
-    public elseGoTo: GoTo = { type: GoToTypes.ERROR },
-    public root: boolean = false
+    public ifGoTo: GoTo = {type: GoToTypes.FIN},
+    public elseGoTo: GoTo = {type: GoToTypes.ERROR},
+    public root: boolean = false,
   ) {
     super();
   }
