76222ac8942c77c640a0b82a5d3cc237ac2fb9c6
amueller <andreas.mueller@exxcellent.de>
2020-11-02 10:36:59 +0100

    change label add to add or update

--- src/components/common/fragments/dropdowns/ActionTypeDropDown.tsx
+++ src/components/common/fragments/dropdowns/ActionTypeDropDown.tsx
@@ -1,6 +1,6 @@
-import React, { FunctionComponent } from "react";
-import { Dropdown, DropdownItemProps, DropdownProps } from "semantic-ui-react";
-import { ActionType } from "../../../../dataAccess/access/types/ActionType";
+import React, {FunctionComponent} from 'react';
+import {Dropdown, DropdownItemProps, DropdownProps} from 'semantic-ui-react';
+import {ActionType} from '../../../../dataAccess/access/types/ActionType';
 
 interface ActionTypeDropDownnProps extends DropdownProps {
   onSelect: (actionType: ActionType | undefined) => void;
@@ -9,14 +9,33 @@ interface ActionTypeDropDownnProps extends DropdownProps {
 }
 
 export const ActionTypeDropDown: FunctionComponent<ActionTypeDropDownnProps> = (props) => {
-  const { onSelect, placeholder, value } = props;
+  const {onSelect, placeholder, value} = props;
   const actions: ActionType[] = Object.values(ActionType);
+
+  const getActionTypeLabel = (type: ActionType) : string => {
+  let label: string = '';
+  switch (type) {
+    case ActionType.ADD:
+      label = 'Add or Update';
+      break;
+    case ActionType.DELETE:
+      label = 'Delete';
+      break;
+    case ActionType.SEND:
+      label = 'Send';
+      break;
+    case ActionType.SEND_AND_DELETE:
+      label = 'Send and delete';
+      break;
+  }
+  return label;
+};
 
   const actionTypeToOption = (actionType: ActionType): DropdownItemProps => {
     return {
       key: actionType,
       value: actionType,
-      text: actionType,
+      text: getActionTypeLabel(actionType),
     };
   };
 
@@ -29,7 +48,7 @@ export const ActionTypeDropDown: FunctionComponent<ActionTypeDropDownnProps> = (
       selectOnBlur={false}
       onChange={(event, data) => onSelect(data.value as ActionType)}
       scrolling
-      placeholder={placeholder || "Select Action Type ..."}
+      placeholder={placeholder || 'Select Action Type ...'}
       value={value}
     />
   );
