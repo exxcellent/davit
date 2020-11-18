d888b703506d86ae9fe0c8a0c404f7c8e9908634
amueller <andreas.mueller@exxcellent.de>
2020-11-18 12:48:23 +0100

    rename cardbutton

--- src/components/common/fragments/buttons/DavitCardButton.tsx
+++ src/components/common/fragments/buttons/DavitCardButton.tsx
@@ -1,14 +1,14 @@
 import React, { FunctionComponent } from 'react';
 import { Icon, SemanticICONS } from 'semantic-ui-react';
 
-interface Carv2CardButtonProps {
+interface DavitCardButtonProps {
     onClick: () => void;
     disable?: boolean;
     icon?: SemanticICONS;
     isActive?: boolean;
 }
 
-export const Carv2CardButton: FunctionComponent<Carv2CardButtonProps> = (props) => {
+export const DavitCardButton: FunctionComponent<DavitCardButtonProps> = (props) => {
     const { onClick, disable, icon, isActive } = props;
 
     return (
