20b66be5c792dff7905cc9188c7569a8d79c8722
amueller <andreas.mueller@exxcellent.de>
2021-03-02 10:14:05 +0100

    remove button group

--- src/components/common/fragments/buttons/DavitButtonGroup.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FunctionComponent } from "react";
-
-interface DavitButtonGroupProps {}
-
-export const DavitButtonGroup: FunctionComponent<DavitButtonGroupProps> = (props) => {
-    const { children } = props;
-
-    return <div className="ButtonGroup">{children}</div>;
-};
