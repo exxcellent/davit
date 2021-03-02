099e7833b350779de375de9ba93978dfff461326
amueller <andreas.mueller@exxcellent.de>
2021-03-02 09:55:03 +0100

    remove unused languages

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { IntlProvider } from "react-intl";
-import { Provider } from "react-redux";
+import {Provider} from "react-redux";
 import "semantic-ui-css/semantic.min.css";
-import { Davit } from "./app/Davit";
-import { store } from "./app/store";
+import {Davit} from "./app/Davit";
+import {store} from "./app/store";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import { languages } from "./translations/languages";
-
-// import { BrowserRouter } from 'react-router-dom';
-// TODO: if we rollout to english countries, this must be redefined (changing the locale "de" to "en" already works)
-/* support variables for languages */
-// const locale = navigator.language;
-// DE
-const locale = "de";
-// EN
-// const locale = 'en';
-const localeData = languages[locale];
 
 ReactDOM.render(
     <Provider store={store}>
-        <IntlProvider locale={locale} messages={localeData}>
-            <Davit />
-        </IntlProvider>
+        <Davit/>
     </Provider>,
     document.getElementById("root"),
 );
