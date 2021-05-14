98bdb38d4bc0bd6def7cf8f4faf48c7d0fe80192
Andreas Müller <andreas.mueller@exxcellent.de>
2021-05-14 09:33:49 +0200

    fixed view navigator style

--- src/components/controllPanel/presentation/fragments/view/fragments/ViewNavigator.tsx
+++ src/components/controllPanel/presentation/fragments/view/fragments/ViewNavigator.tsx
@@ -1,10 +1,10 @@
-import React, { FunctionComponent } from 'react';
-import { DavitButton } from '../../../../../common/fragments/buttons/DavitButton';
-import { faFastBackward } from '@fortawesome/free-solid-svg-icons/faFastBackward';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
-import { faFastForward } from '@fortawesome/free-solid-svg-icons/faFastForward';
-import { CenteredFlex } from '../../../../../common/fragments/CenteredFlex';
+import React, {FunctionComponent} from 'react';
+import {DavitButton} from '../../../../../common/fragments/buttons/DavitButton';
+import {faFastBackward} from '@fortawesome/free-solid-svg-icons/faFastBackward';
+import {faArrowLeft} from '@fortawesome/free-solid-svg-icons/faArrowLeft';
+import {faArrowRight} from '@fortawesome/free-solid-svg-icons/faArrowRight';
+import {faFastForward} from '@fortawesome/free-solid-svg-icons/faFastForward';
+import {CenteredFlex} from '../../../../../common/fragments/CenteredFlex';
 
 interface ViewNavigatorProps {
     fastBackward: () => void,
@@ -15,36 +15,35 @@ interface ViewNavigatorProps {
 }
 
 export const ViewNavigator: FunctionComponent<ViewNavigatorProps> = (props) => {
-    const { fastBackward, fastForward, backward, forward, index } = props;
+    const {fastBackward, fastForward, backward, forward, index} = props;
 
     return (
         <CenteredFlex>
             <DavitButton
                 iconName={faFastBackward}
                 onClick={fastBackward}
-                style={{ borderTopRightRadius: 0, borderBottomRightRadius: 0 }}
-                className={"padding"}
+                style={{borderTopRightRadius: 0, borderBottomRightRadius: 0}}
             />
             <DavitButton
                 iconName={faArrowLeft}
                 label='BACK'
                 onClick={backward}
                 iconLeft={true}
-                style={{ borderRadius: 0 }}
+                style={{borderRadius: 0}}
             />
-            <div className={'border'} style={{textAlign: "center", padding: "0.55em", borderRadius: 0}}>
+            <div className={'border'} style={{textAlign: "center", padding: "0.43rem", borderRadius: 0}}>
                 <label>{index}</label>
             </div>
             <DavitButton
                 iconName={faArrowRight}
                 label='NEXT'
                 onClick={forward}
-                style={{ borderRadius: 0 }}
+                style={{borderRadius: 0}}
             />
             <DavitButton
                 iconName={faFastForward}
                 onClick={fastForward}
-                style={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0 }}
+                style={{borderTopLeftRadius: 0, borderBottomLeftRadius: 0}}
             />
         </CenteredFlex>
     );
