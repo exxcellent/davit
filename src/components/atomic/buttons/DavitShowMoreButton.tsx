f6cdbbdb3a1b09b0755462025b9c549fced31559
Andreas Müller <andreas.mueller@exxcellent.de>
2021-07-22 16:21:23 +0200

    fixed show more button

--- src/components/atomic/buttons/DavitShowMoreButton.tsx
+++ src/components/atomic/buttons/DavitShowMoreButton.tsx
@@ -1,32 +1,21 @@
 import { faAngleDown } from "@fortawesome/free-solid-svg-icons/faAngleDown";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons/faAngleRight";
-import React, { FunctionComponent, useEffect, useState } from "react";
+import React, { FunctionComponent } from "react";
 import { ElementSize } from "../../../style/Theme";
-import { DavitButtonProps } from "./DavitButton";
 import { DavitIconButton } from "./DavitIconButton";
 
-interface DavitMoreButtonProps extends DavitButtonProps {
-    show?: boolean
+interface DavitMoreButtonProps {
+    onClick: (show: boolean) => void;
+    show?: boolean;
+    className?: string;
 }
 
 export const DavitShowMoreButton: FunctionComponent<DavitMoreButtonProps> = (props) => {
-    const {onClick, show} = props;
+    const {onClick, show, className} = props;
 
-    const [showMore, setShowMore] = useState(false);
-
-    useEffect(() => {
-        if (show !== undefined) {
-            setShowMore(show);
-        }
-    }, [show]);
-
-    const onToggle = () => {
-        setShowMore(!showMore);
-        onClick();
-    };
-
-    return <DavitIconButton onClick={onToggle}
+    return <DavitIconButton onClick={() => onClick(!show)}
                             size={ElementSize.tiny}
-                            iconName={showMore ? faAngleDown : faAngleRight}
+                            iconName={show ? faAngleDown : faAngleRight}
+                            className={className}
     />;
 };
