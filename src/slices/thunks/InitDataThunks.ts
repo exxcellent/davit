1f1a78e013f0f1cc1ad9c61fa8ce334bdcb8d158
Andreas Müller <andreas.mueller@exxcellent.de>
2021-07-27 10:58:14 +0200

    initDataThunk remove set to edit

--- src/slices/thunks/InitDataThunks.ts
+++ src/slices/thunks/InitDataThunks.ts
@@ -2,7 +2,6 @@ import { InitDataTO } from "../../dataAccess/access/to/InitDataTO";
 import { DataAccess } from "../../dataAccess/DataAccess";
 import { DataAccessResponse } from "../../dataAccess/DataAccessResponse";
 import { AppThunk } from "../../store";
-import { editActions, EditActions, Mode } from "../EditSlice";
 import { GlobalActions } from "../GlobalSlice";
 import { MasterDataActions } from "../MasterDataSlice";
 
@@ -11,7 +10,6 @@ const saveInitDataThunk = (initData: InitDataTO): AppThunk => (dispatch) => {
     if (response.code !== 200) {
         dispatch(GlobalActions.handleError(response.message));
     }
-    dispatch(EditActions.setMode.editInitData(response.object));
 };
 
 const deleteInitDataThunk = (initDataId: number): AppThunk => (dispatch) => {
@@ -22,18 +20,7 @@ const deleteInitDataThunk = (initDataId: number): AppThunk => (dispatch) => {
     dispatch(MasterDataActions.loadDataSetupsFromBackend());
 };
 
-const setInitDataToEditThunk = (initData: InitDataTO): AppThunk => (dispatch, getState) => {
-    const mode: Mode = getState().edit.mode;
-
-    if (mode === Mode.EDIT_DATASETUP_INITDATA) {
-        dispatch(editActions.setInitDataToEdit(initData));
-    } else {
-        dispatch(GlobalActions.handleError("Try to set initData to edit in mode: " + mode));
-    }
-};
-
 export const EditInitData = {
     save: saveInitDataThunk,
     delete: deleteInitDataThunk,
-    update: setInitDataToEditThunk,
 };
