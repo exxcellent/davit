b3bb1c2afb80454953b827ccc62f02c5fff7aa38
Andreas Müller <andreas.mueller@exxcellent.de>
2021-08-04 16:19:52 +0200

    remove data-setup initdata and migrator

--- src/dataAccess/access/to/DataSetupTO.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractTO } from "./AbstractTO";
-
-export class DataSetupTO extends AbstractTO {
-    constructor(public name = "", public note: string = "") {
-        super();
-    }
-}
