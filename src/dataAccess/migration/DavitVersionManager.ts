043c9ae92165de05156638a7974cfc7ff1be5e44
Andreas Müller <andreas.mueller@exxcellent.de>
2021-08-05 09:38:38 +0200

    migrator disabled

--- src/dataAccess/migration/DavitVersionManager.ts
+++ src/dataAccess/migration/DavitVersionManager.ts
@@ -1,8 +1,5 @@
 import { DAVIT_VERISON } from "../../DavitConstants";
 import { StoreTO } from "../access/to/StoreTO";
-import { DavitVersionMigrator01 } from "./davitVersio01/DavitVersionMigrator01";
-import { DavitVersionMigrator02 } from "./davitVersion02/DavitVersionMigrator02";
-import { DavitVersionMigrator03 } from "./davitVersion03/DavitVersionMigrator03";
 
 export const DavitVersionManager = {
     updateProject(dataStoreObject: StoreTO): StoreTO {
@@ -11,18 +8,18 @@ export const DavitVersionManager = {
 
         switch (dataStoreObject.version) {
             case undefined:
-                migratedDataStoreObject = DavitVersionMigrator03.migrate(
-                    DavitVersionMigrator02.migrate(
-                        DavitVersionMigrator01.migrate(dataStoreObject),
-                    )
-                );
-                break;
-            case 0.1:
-                migratedDataStoreObject = DavitVersionMigrator03.migrate(
-                    DavitVersionMigrator02.migrate(dataStoreObject));
-                break;
-            case 0.2:
-                migratedDataStoreObject = DavitVersionMigrator03.migrate(dataStoreObject);
+            //     migratedDataStoreObject = DavitVersionMigrator03.migrate(
+            //         DavitVersionMigrator02.migrate(
+            //             DavitVersionMigrator01.migrate(dataStoreObject),
+            //         )
+            //     );
+            //     break;
+            // case 0.1:
+            //     migratedDataStoreObject = DavitVersionMigrator03.migrate(
+            //         DavitVersionMigrator02.migrate(dataStoreObject));
+            //     break;
+            // case 0.2:
+            //     migratedDataStoreObject = DavitVersionMigrator03.migrate(dataStoreObject);
         }
 
         return migratedDataStoreObject;
